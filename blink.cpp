3ae636399fc35028669327cc217e5296480b1af5
22Xc <49470219+22Xc@users.noreply.github.com>
2021-06-08 16:50:19 +0800

    Add files via upload

---
+++ blink.cpp
@@ -0,0 +1,52 @@
+/*
+blink.cpp - Simple example in creating your own Arduino Library
+Copyright (c) op from TMM. All right reserved.
+
+*/
+ 
+#include <Arduino.h>
+#include "blink.h"
+
+Blink::Blink(int pin){
+ pinMode(pin, OUTPUT);
+ pinNumber = pin; 
+}
+ 
+void Blink::blink(bool value){
+ if(value == true){
+	 for (int a = 0; a <= 255; a++) { //循环语句，控制PWM亮度的增加
+		 analogWrite(pinNumber, a);
+		 delay(8); //当前亮度级别维持的时间,单位毫秒
+	 }
+	 for (int a = 255; a >= 0; a--) { //循环语句，控制PWM亮度减小
+		 analogWrite(pinNumber, a);
+		 delay(8); //当前亮度的维持的时间,单位毫秒
+	 }
+ }else{
+ digitalWrite(pinNumber,LOW);
+ }
+}
+
+void Blink::blink(bool value, int blinkLength ){
+ if(value == true){
+ digitalWrite(pinNumber,HIGH);
+ delay(blinkLength);
+ digitalWrite(pinNumber,LOW);
+ delay(blinkLength);
+ }else{
+ digitalWrite(pinNumber,LOW);
+ }
+}
+ 
+void Blink::blink(bool value, int blinkLength, int blinkLoops){
+ if(value == true){
+ for(int i=0;i<blinkLoops;i++){
+ digitalWrite(pinNumber,HIGH);
+ delay(blinkLength);
+ digitalWrite(pinNumber,LOW);
+ delay(blinkLength);
+ }
+ }else{
+ digitalWrite(pinNumber,LOW);
+ }
+}
